cb023d1453d146121766221041979c5072941b47
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2022-07-05 09:29:24 +0700

    Impl of getting dates from today and yesterday

---
+++ app/src/main/java/com/alexeyyuditsky/exchange_rates/utils/Dates.kt
@@ -0,0 +1,19 @@
+package com.alexeyyuditsky.exchange_rates.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+val currentDate by lazy { currentDate() }
+val yesterdayDate by lazy { yesterdayDate() }
+
+fun currentDate(): String {
+    val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    return sdf.format(Date())
+}
+
+fun yesterdayDate(): String {
+    val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    val cal = Calendar.getInstance()
+    cal.add(Calendar.DATE, -1)
+    return sdf.format(cal.time)
+}
