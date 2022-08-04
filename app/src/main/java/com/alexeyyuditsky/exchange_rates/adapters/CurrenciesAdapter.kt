3950949e776c839b01a4e7ab04ac9f27d64e0e5f
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2022-08-04 20:03:49 +0700

    Refactoring currency adapter

--- app/src/main/java/com/alexeyyuditsky/exchange_rates/adapters/CurrenciesAdapter.kt
+++ app/src/main/java/com/alexeyyuditsky/exchange_rates/adapters/CurrenciesAdapter.kt
@@ -1,6 +1,5 @@
 package com.alexeyyuditsky.exchange_rates.adapters
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -12,27 +11,22 @@ import androidx.recyclerview.widget.RecyclerView
 import com.alexeyyuditsky.exchange_rates.R
 import com.alexeyyuditsky.exchange_rates.databinding.ItemCurrencyBinding
 import com.alexeyyuditsky.exchange_rates.model.currencies.Currency
+import com.alexeyyuditsky.exchange_rates.utils.currencyImagesMap
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 
-class CurrenciesAdapter : PagingDataAdapter<Currency, CurrenciesAdapter.Holder>(UsersDiffCallback()) {
+class CurrenciesAdapter : PagingDataAdapter<Currency, CurrenciesAdapter.Holder>(CurrenciesDiffCallback()) {
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val currency = getItem(position) ?: return
         val context = holder.itemView.context
         with(holder.binding) {
-            currencyImageView.setImageResource(getImageId(context, currency.shortName))
-            //setCurrencyImageView(context, currency.shortName, currencyImageView)
             currencyShortNameTextView.text = currency.shortName
             currencyFullNameTextView.text = currency.fullName
             currencyValueTextView.text = currency.valueToday
             currencyDifferenceTextView.text = currency.valueDifference
+            setCurrencyImage(context, currency.shortName, currencyImageView)
+            setCurrencyColor(context, currency.valueDifference, currencyDifferenceTextView)
         }
-        setColorForCurrencyDifferenceTextView(
-            context,
-            currency.valueDifference,
-            holder.binding.currencyDifferenceTextView,
-        )
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
@@ -41,22 +35,15 @@ class CurrenciesAdapter : PagingDataAdapter<Currency, CurrenciesAdapter.Holder>(
         return Holder(binding)
     }
 
-    private fun getImageId(context: Context, shortName: String): Int {
-        val id = context.resources.getIdentifier(shortName.lowercase(), "drawable", context.packageName)
-        return if (id == 0) R.drawable.tryy else id
-    }
-
-    private fun setCurrencyImageView(context: Context, imageViewName: String, currencyImageView: ImageView) {
-        val res = R.drawable.afn
+    private fun setCurrencyImage(context: Context, shortName: String, currencyImageView: ImageView) {
         Glide.with(context)
-            .load(res)
+            .load(currencyImagesMap[shortName] ?: R.drawable.ic_error)
             .placeholder(R.drawable.ic_temp)
             .error(R.drawable.ic_error)
             .into(currencyImageView)
     }
 
-    @SuppressLint("NewApi")
-    private fun setColorForCurrencyDifferenceTextView(context: Context, value: String, textView: TextView) {
+    private fun setCurrencyColor(context: Context, value: String, textView: TextView) {
         if (value.toFloat() > 0f) textView.setTextColor(context.getColor(R.color.green))
         else if (value.toFloat() < 0f) textView.setTextColor(context.getColor(R.color.red))
         else textView.setTextColor(context.getColor(android.R.color.tab_indicator_text))
@@ -66,7 +53,7 @@ class CurrenciesAdapter : PagingDataAdapter<Currency, CurrenciesAdapter.Holder>(
 
 }
 
-class UsersDiffCallback : DiffUtil.ItemCallback<Currency>() {
+class CurrenciesDiffCallback : DiffUtil.ItemCallback<Currency>() {
     override fun areItemsTheSame(oldItem: Currency, newItem: Currency): Boolean = oldItem.id == newItem.id
     override fun areContentsTheSame(oldItem: Currency, newItem: Currency): Boolean = oldItem == newItem
 }
